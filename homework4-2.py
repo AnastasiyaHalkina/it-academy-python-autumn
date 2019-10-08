eeb5a4e8a2d38ffa6e00aa81f41562b6a67ef473
Anastasiya H <schwarzer300708held@gmail.com>
2019-10-08 17:28:00 +0300

    homework4

--- homework4-2.py
+++ homework4-2.py
@@ -1,21 +1,24 @@
 import copy
+
 # Use a list comprehension to construct the list
 # ['ab', 'ac', 'ad', 'bb', 'bc', 'bd']
-lst = [chr(97 + i) + chr(98 + j) for i in range(2) for j in range(3)]
-print(lst)
+lst = [x + y for x in ['a', 'b'] for y in ['b', 'c', 'd']]
+print('Task 1:', lst)
+
 # Use a slice on the above list to construct the list ['ab', 'ad', 'bc']
 lst1 = lst[::2]
-print(lst1)
+print('Task 2:', lst1)
+
 # Use a list comprehension to construct the list ['1a', '2a', '3a', '4a']
-lst = [str(el + 1) + 'a' for el in range(4)]
-print(lst)
+lst = [str(el) + 'a' for el in range(1, 5)]
+print('Task 3:', lst)
+
 # Simultaneously remove the element '2a' from the above list and print it.
-lst.remove('2a')
-print(lst)
+print('Task 4:', lst.pop(1))
+
 """ Copy the above list and add '2a' back into the list such
 that the original is still missing it."""
 lst2 = copy.copy(lst)
 lst2[1] = '2a'
 lst2 = lst2[:2] + lst[1:]
-print(lst2)
-print(lst)
+print('Task 5:', 'lst2 =', lst2, 'lst = ', lst)
--- homework4-3.py
+++ homework4-3.py
@@ -1,27 +1,27 @@
 # Create the list ['a', 'b', 'c'], then create a tuple from that list
-lst = [chr(97 + i) for i in range(3)]
-print(lst)
+lst = [x for x in 'abc']
 t = tuple(lst)
-print(t)
+print('Task 1:', lst, t)
+
 # Create the tuple ('a', 'b', 'c'),
 # then create a list from that tuple.
 # (Hint: the material needed to do this has been covered,
 # but it's not entirely obvious)
 t1 = ()
-for i in range(3):
-    t1 += (chr(97 + i), )
-print(t1)
+for x in 'abc':
+    t1 += (x, )
 lst = list(t1)
-print(lst)
+print('Task 2:', t, lst)
+
 # Make the following instantiations simultaneously:
 # a = 'a', b=2, c='gamma'. (That is, in one line of code).
 a, b, c = 'a', 2, 'gamma'
-print(a, b, c)
+print('Task 3:', a, b, c)
+
 # Create a tuple containing just a single element
 # which in turn contains the three elements 'a', 'b', and 'c'.
 # Verify that the length is actually 1 by using the len() function.
-lst = [chr(97 + i) for i in range(3)]
+lst = [x for x in 'abc']
 t2 = (lst, )
 n = len(t2)
-print(t2)
-print(n)
+print('Task 4:', t2, 'len(t2) = ', n)
